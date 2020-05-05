ecf4b93b7376b110e81ed7ac57d0bc05d159d689
jackmorrison <jsm1317@ic.ac.uk>
2020-05-06 00:13:58 +0100

    Updates project sources in yml file

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blogs`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
