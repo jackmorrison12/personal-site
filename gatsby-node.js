8af87a20ad572974399f6a029e47c5a0471a2e8d
jackmorrison <jsm1317@ic.ac.uk>
2020-05-06 01:46:20 +0100

    Adds article pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,7 @@ exports.createPages = async ({actions, graphql, reporter}) => {
     {
       allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
+        limit: 1000, filter: {fileAbsolutePath: {regex: "/blog/"  }}
       ) {
         edges {
           node {
@@ -44,3 +44,40 @@ exports.createPages = async ({actions, graphql, reporter}) => {
     })
   })
 };
+
+exports.createPages = async ({actions, graphql, reporter}) => {
+    const {createPage} = actions;
+  
+    const articleTemplate = path.resolve(`src/templates/article.js`);
+  
+    const result = await graphql(`
+      {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          limit: 1000, filter: {fileAbsolutePath: {regex: "/articles/"  }}
+        ) {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `);
+  
+    // Handle errors
+    if (result.errors) {
+      reporter.panicOnBuild(`Error while running GraphQL query.`);
+      return
+    }
+  
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.frontmatter.slug,
+        component: articleTemplate,
+        context: {}, // additional data can be passed via context
+      })
+    })
+  };
