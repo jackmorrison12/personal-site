992522757f56ebfaeaa5cecc56b9aa4f1d4634f7
jackmorrison <jsm1317@ic.ac.uk>
2020-05-06 19:55:04 +0100

    Display credits from sources file

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import sources from "../data/sources.json"
 
 const CreditsPage = () => (
   <Layout>
@@ -12,21 +13,38 @@ const CreditsPage = () => (
             <div className="col-xs-12 ">
                 <h1 className="is-hero-menu margin-0-t">Credits</h1>
                 <p>This site would not be possible without the following code & tutorials</p>
-                <h2>Framework</h2>
-                    <ul>
-                        <li>Gatsby Tutorial - <a href="https://www.gatsbyjs.org/tutorial/" className="is-medium-blue btn">Source</a></li>
-                    </ul>
-                <h2>Styling</h2>
-                    <ul>
-                        <li>Design Inspiration - <a href="https://sld.codes" className="is-medium-blue btn">Source</a></li>
-                        <li>CSS Skeletons - <a href="https://github.com/slarsendisney/personal-site/tree/master/src/styles" className="is-medium-blue btn">Source</a></li>
-                        <li>Dark Mode Support - <a href="https://sld.codes/articles/How-I-Added-Dark-Mode-In-20-Lines" className="is-medium-blue btn">Source</a></li>
-                    </ul>
-                <h2>Hosting</h2>
-                    <ul>
-                        <li>Netlify Tutorial - <a href="https://www.netlify.com/blog/2016/02/24/a-step-by-step-guide-gatsby-on-netlify/" className="is-medium-blue btn">Source</a></li>
-                    </ul>
-                <h2>CMS</h2>
+                {
+                    sources.map((item) => {
+                        return(
+                            <>
+                                <h2>{item.type}</h2>
+                                <ul>
+                                {item.sources.map((item) => {
+                                    return(
+                                    <li>{item.name} - 
+                                        
+                                        { 
+                                        item.links.length === 1 
+                                        ? 
+                                            <>{" "}<a href={item.links[0]} className="is-medium-blue btn">Source</a></>
+                                        :
+                                            item.links.map((link, i, links) => {
+                                                return(
+                                                    links.length - 1 === i 
+                                                    ?
+                                                        <>{" "}<a href={link} className="is-medium-blue btn">Source {i+1}</a></>
+                                                    :
+                                                        <>{" "}<a href={link} className="is-medium-blue btn">Source {i+1},</a></>
+                                                );
+                                            })}
+                                    </li>
+                                    );
+                                })}
+                                </ul>
+                            </>
+                        );
+                    })
+                }
             </div>
         </div>
     </div>
@@ -34,7 +52,19 @@ const CreditsPage = () => (
         <div className="row container ">
             <div className="col-xs-12 ">
                 <h1 className="is-hero-menu margin-0-t">Site Structure</h1>
-                <div className="line margin-3-t margin-10-b" />
+                <p>
+                    As you can see from above, the site is created using GatsbyJS, a site generator based on react. It uses custom-built CSS from many sources listed above, is hosted using Netlify, and the content is manages using Netlify CMS.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div className="is-grey is-light-grey-bg pad-10-tb pad-3-lr">
+        <div className="row container ">
+            <div className="col-xs-12 ">
+                <h1 className="is-hero-menu margin-0-t">Can I use it?</h1>
+                <p>
+                    Of course! This site is completely open source. All code is available in a git repository, which includes instructions on how to set up and deploy the site. All you have to do is put a link back to this site (this page in particular) so that everyone who I have sourced from also has credit!
+                </p>
             </div>
         </div>
     </div>
