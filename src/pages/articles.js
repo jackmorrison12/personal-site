b32723cb283b4d205bfd581990883901957b18eb
jackmorrison <jsm1317@ic.ac.uk>
2020-05-06 02:11:34 +0100

    Displays articles on correct page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,10 +3,13 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ArticleRoll from '../components/articles/articleroll'
+
 
 const ArticlesPage = () => (
   <Layout>
     <SEO title="Articles" />
+    <ArticleRoll />
   </Layout>
 )
 
