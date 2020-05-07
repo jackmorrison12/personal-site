f4ba42534ba3b5f62f2527922a597f82be8f350c
jackmorrison <jsm1317@ic.ac.uk>
2020-05-07 15:48:38 +0100

    Updates light grey colour

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,7 +11,7 @@ const ProjectsPage = () => (
     <div className="is-grey is-light-grey-bg pad-10-tb pad-3-lr">
       <div className="row container ">
         <div className="col-xs-12 ">
-          <h1 className="is-hero-menu margin-0-t">Some cool projects I've worked on</h1>
+          <h1 className="is-hero-menu margin-0-t">Some stuff I've made</h1>
           <div className="line margin-3-t margin-10-b" />
         </div>
         <div className="col-xs-12 col-md-12">
