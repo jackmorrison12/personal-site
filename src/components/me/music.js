292f172f258db517c0ba6ce8616b96eff625fdae
jackmorrison <jsm1317@ic.ac.uk>
2020-05-13 01:50:20 +0100

    Adds music component

--- src/components/me/music.js
+++ src/components/me/music.js
@@ -1,5 +1,38 @@
 import React from "react"
+import ReactAudioPlayer from 'react-audio-player';
+
+import musicData from "../../data/legacy/music.json"
 
 export default () => (
-    <h1>Music</h1>
+    <>
+    <h1  className="col-xs-12"><span role="img" aria-label="guitar">🎸</span> Making Music</h1>
+    <div className="col-xs-12 col-md-6">
+        
+        <p>
+            I've always loved listening to music, and in 2019 I decided I wanted to learn how to make it too. I took a course called <a className="btn" href="https://www.imperial.ac.uk/horizons/module-options/2nd-year-undergraduates/music-technology/">Music Technology</a>.
+            <br />
+            We covered many topics, starting off with the basics of how to use a Digital Audio Workstation, such as creating sounds using software instruments and MIDI keyboards. We then moved on to learn things like mixing and using inserts and sends.
+            <br />
+            We used Cubase in class, however due to coronavirus, I had to teach myself how to use Logic Pro X in order to be able to create and submit my work.
+            <br />
+            For the course assessment, we had to create two compositions. You can find both of mine on this page, as well as some other things I created along the way!
+        </p>
+    </div>
+    <div className="col-xs-12 col-md-6 text-align-center">        
+        {
+            musicData.map((vid, i) => {
+                return (  
+                    <div className="text-align-right">         
+                        <h4>{vid.name}</h4>                 
+                        <ReactAudioPlayer
+                            src={vid.filename}
+                            className="width-full"
+                            controls
+                            />
+                    </div>
+                );
+            })
+        }
+    </div>
+</>
 )
