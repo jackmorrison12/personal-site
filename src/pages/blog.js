dc55f73bfb2ef8353852f18ed18bbbf8d333167d
jackmorrison <jsm1317@ic.ac.uk>
2020-05-14 00:34:28 +0100

    Updates blog title

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,7 +11,7 @@ const BlogPage = () => (
     <div className="is-grey is-light-grey-bg pad-10-tb pad-3-lr">
       <div className="row container ">
         <div className="col-xs-12 ">
-          <h1 className="is-hero-menu margin-0-t">My Daily Ramblings...</h1>
+          <h1 className="is-hero-menu margin-0-t">My Blog</h1>
           <div className="line margin-3-t margin-10-b" />
         </div>
         <div className="col-xs-12 col-md-12">
