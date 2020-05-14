f0f24307077525c42bcfef5442929a4325011c03
jackmorrison <jsm1317@ic.ac.uk>
2020-05-14 02:58:24 +0100

    Tries to fix compilation issue on webpack

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,21 @@
 
 const path = require(`path`);
 
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
+
 exports.createPages = async ({actions, graphql, reporter}) => {
   const {createPage} = actions;
 
