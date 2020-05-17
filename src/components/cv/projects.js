e8752a229966de8fcabdd3b3e906caf3c59c888b
jackmorrison <jsm1317@ic.ac.uk>
2020-05-17 02:45:57 +0100

    Links in CV aren't relative anymore

--- src/components/cv/projects.js
+++ src/components/cv/projects.js
@@ -7,7 +7,7 @@ export default (props) => {
             {
                 props.projects.nodes.map((item) => (
                     <div className="pad-3-b">
-                        <a href={item.frontmatter.slug}><h4 className="margin-0-b is-black-always">{item.frontmatter.title}</h4></a>
+                        <a href={"https://jackmorrison.xyz"+item.frontmatter.slug}><h4 className="margin-0-b is-black-always">{item.frontmatter.title}</h4></a>
                         <p className="margin-0-t">{item.frontmatter.startdate} - {item.frontmatter.enddate}</p>
                         {
                             item.frontmatter.highlights.map((highlight) => (
