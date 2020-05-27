d7641b69ecb221162321fe1402c66159d2ecd2a7
jackmorrison <jsm1317@ic.ac.uk>
2020-05-27 02:16:42 +0100

    Adds dark mode toggle

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,34 +8,51 @@
 import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql, Link } from "gatsby"
+import useDarkMode from "use-dark-mode"
 import { Link } from "gatsby"
 
 import Header from "./header"
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
+  const darkMode = useDarkMode(true)
+  function switchDarkMode() {
+    var toggle = document.getElementById("dark-mode-switch")
+    if (toggle.checked) {
+      darkMode.enable()
+    } else {
+      darkMode.disable()
+    }
+  }
 
   return (
     <>
       <Header />
       <main className="is-light-grey-bg is-black">{children}</main>
       <footer className="is-white-bg is-black pad-2">
-        Created by me, with help from{" "}
-        <Link
-          to="/credits"
-          activeClassName="is-black"
-          className="is-medium-blue link"
-        >
-          these people
-        </Link>
+        <div className="dark-toggle">
+          Light{"    "}
+          <label class="switch">
+            <input
+              type="checkbox"
+              id="dark-mode-switch"
+              onClick={() => switchDarkMode()}
+              checked={darkMode.value}
+            />
+            <span class="slider round"></span>
+          </label>
+          {"    "}
+          Dark
+        </div>
+        <div>
+          Created by me, with help from{" "}
+          <Link
+            to="/credits"
+            activeClassName="is-black"
+            className="is-medium-blue link"
+          >
+            these people
+          </Link>
+        </div>
       </footer>
     </>
   )
