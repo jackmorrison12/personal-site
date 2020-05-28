c36279be636ee1d5d08617219cee50db473e4887
jackmorrison <jsm1317@ic.ac.uk>
2020-05-29 00:42:03 +0100

    Removes CV page title

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -16,7 +16,6 @@ class CVPage extends React.Component {
         <div className="is-grey is-light-grey-bg pad-10-tb pad-3-lr">
           <div className="row container ">
             <div className="col-xs-12 ">
-              <h1 className="is-title margin-0-t">CV</h1>
               <div className="mobile-hide" ref={cv => (this.cv = cv)}>
                 <CV />
               </div>
