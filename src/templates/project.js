41d0d2d88abdc96785a020a765af87781a68ffc1
jackmorrison <jsm1317@ic.ac.uk>
2020-06-05 03:13:36 +0100

    Updates project SEO with image and description

--- src/templates/project.js
+++ src/templates/project.js
@@ -18,7 +18,11 @@ export default function Template({
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
-      <SEO title={frontmatter.title} />
+      <SEO
+        title={frontmatter.title}
+        image={"/img/" + frontmatter.banner.childImageSharp.fluid.originalName}
+        description={frontmatter.description}
+      />
       <div className="is-grey is-light-grey-bg">
         <div className="row container pad-10-t ">
           <div className="col-xs-12 pad-3-lr">
@@ -144,6 +148,7 @@ export const pageQuery = graphql`
           childImageSharp {
             fluid(maxWidth: 1000) {
               ...GatsbyImageSharpFluid
+              originalName
             }
           }
         }
