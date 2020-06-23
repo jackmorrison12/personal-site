d8c897e44b31501eae2a36d7003604274c678a4a
jackmorrison <jsm1317@ic.ac.uk>
2020-06-23 23:23:43 +0100

    Updates the page description on data page

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,7 @@ import GitStats from "../components/stats/git-stats"
 import LastFmStats from "../components/stats/last-fm-stats"
 
 import useLastBuildData from "../hooks/use-last-build-data"
+import { OutboundLink } from "gatsby-plugin-google-analytics"
 
 const DataPage = () => {
   var last_build_time = useLastBuildData()
@@ -48,12 +50,39 @@ const DataPage = () => {
             <h1>What is this page?</h1>
             <p>
               I love data, and I also love creating things. I wanted to combine
-              loads of APIs I have access to and see what I could create.{" "}
+              loads of APIs I have access to and see what I could create.
             </p>
             <p>
-              This page is generated on every site build, running scripts
-              locally using git hooks, and on the build machine using a shell
-              script.
+              I continued using APIs as I had from the{" "}
+              <OutboundLink
+                className="link"
+                href="https://v3.jackmorrison.xyz/me"
+              >
+                previous version of my site
+              </OutboundLink>
+              , but since I was statically generating my site, I didn't need to
+              limit myself to using only APIs. I saw an example on{" "}
+              <OutboundLink className="link" href="https://sld.codes/stats">
+                sld.codes
+              </OutboundLink>{" "}
+              where Sam used scripts to generate stats on each site build, and
+              decided to try implementing something similar myself.
+            </p>
+            <p>
+              However, I then also wanted to be able to make the stats work in
+              real time, rather than just on each build, which is why I am
+              currently working on{" "}
+              <Link className="link" to="/live">
+                a new iteration
+              </Link>{" "}
+              of this page, which connects to my own NodeJS server to provide
+              real-time stats! This is a WIP page that I hope to integrate
+              gradually into this page.
+            </p>
+            <p>
+              As of now, this page is generated on every site build, running
+              scripts locally using git hooks, and on the build machine using a
+              shell script.
             </p>
           </div>
         </div>
