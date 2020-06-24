4a0ae0760da80c9914b03f2264cba0bdff377ff7
jackmorrison <jsm1317@ic.ac.uk>
2020-06-24 15:20:25 +0100

    Updates homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
+import Img from "gatsby-image"
+import { Twemoji } from "react-emoji-render"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -15,92 +17,82 @@ import ProjectPreviews from "../components/projects/projectpreviews"
 import socials from "../data/socials.json"
 library.add(fab)
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className=" is-light-grey-bg is-black">
-      <div className="row container-small pad-20-t pad-10-b">
-        <div className="col-xs-12 col-sm-6 pad-5-lr">
-          <h1 className="is-title margin-0">Jack Morrison</h1>
-          <h3 className="is-subtitle">
-            Computing Student @{" "}
-            <OutboundLink
-              className="is-red link"
-              href="https://www.imperial.ac.uk/computing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Imperial
-            </OutboundLink>
-          </h3>
-          <div className="line margin-10-t margin-10-b" />
-          <div className="">
-            {socials
-              .filter(item => item.home)
-              .map(item => (
+export default () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className=" is-black diagonal-box">
+        <div className="row container-small pad-10-t">
+          <div className="col-xs-12 pad-5-lr">
+            <div className="">
+              <h1 className="me-hero-title is-title margin-0-t">
+                <Twemoji svg text="Hey 👋 I'm Jack" />
+                <br />
+                I'm a computing student at{" "}
                 <OutboundLink
-                  href={item.url}
-                  className="is-black pad-2 col-xs-3"
+                  className="is-red link"
+                  href="https://www.imperial.ac.uk/computing"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <FontAwesomeIcon
-                    icon={["fab", item.icon]}
-                    size="2x"
-                    className="grow-5"
-                  />
+                  Imperial
                 </OutboundLink>
-              ))}
+              </h1>
+              <h3>
+                Feel free to take a look around and discover some things I've
+                been working on!
+              </h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="is-red-bg is-black diagonal-box">
-      <div className="row container-small pad-20-t pad-3-lr pad-20-b content">
-        <div className="col-xs-12">
-          <h2>Projects I've been working on</h2>
-          <ProjectPreviews />
-        </div>
-      </div>
-    </div>
-    <div className="is-light-grey-bg is-black diagonal-box">
-      <div className="row container-small pad-20-t pad-3-lr pad-20-b content">
-        <div className="col-xs-12">
-          <h2>My Latest Articles</h2>
-          <ArticlePreviews />
-        </div>
-      </div>
-    </div>
-    <div className="is-red-bg is-black diagonal-box">
-      <div className="row container-small pad-20-t pad-3-lr pad-20-b content">
-        <div className="col-xs-12">
-          <h2>My Latest Blog Posts</h2>
-          <BlogPreviews />
-        </div>
-      </div>
-    </div>
-    <div className="is-light-grey-bg is-black ">
-      <div className="row container-small pad-20-t pad-3-lr pad-10-b ">
-        <div className="col-xs-12">
-          <h2 className="text-align-center">Where else can you reach me?</h2>
-          <div className="text-align-center">
-            {socials
-              .filter(item => item.home)
-              .map(item => (
-                <OutboundLink
-                  href={item.url}
-                  className="is-black pad-2 col-xs-3"
-                >
-                  <FontAwesomeIcon
-                    icon={["fab", item.icon]}
-                    size="2x"
-                    className="grow-5"
-                  />
-                </OutboundLink>
-              ))}
+      <div className="is-red-bg is-black diagonal-box">
+        <div className="row container-small pad-20-t pad-3-lr pad-20-b content">
+          <div className="col-xs-12">
+            <h2>Projects I've been working on</h2>
+            <ProjectPreviews />
           </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
-
-export default IndexPage
+      <div className="is-light-grey-bg is-black diagonal-box">
+        <div className="row container-small pad-20-t pad-3-lr pad-20-b content">
+          <div className="col-xs-12">
+            <h2>My Latest Articles</h2>
+            <ArticlePreviews />
+          </div>
+        </div>
+      </div>
+      <div className="is-red-bg is-black diagonal-box">
+        <div className="row container-small pad-20-t pad-3-lr pad-20-b content">
+          <div className="col-xs-12">
+            <h2>My Latest Blog Posts</h2>
+            <BlogPreviews />
+          </div>
+        </div>
+      </div>
+      <div className="is-light-grey-bg is-black ">
+        <div className="row container-small pad-20-t pad-3-lr pad-10-b ">
+          <div className="col-xs-12">
+            <h2 className="text-align-center">Where else can you reach me?</h2>
+            <div className="text-align-center">
+              {socials
+                .filter(item => item.home)
+                .map(item => (
+                  <OutboundLink
+                    href={item.url}
+                    className="is-black pad-2 col-xs-3"
+                  >
+                    <FontAwesomeIcon
+                      icon={["fab", item.icon]}
+                      size="2x"
+                      className="grow-5"
+                    />
+                  </OutboundLink>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
