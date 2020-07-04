b1c1cb0a2a2e7ada337e8659c8d8ca4971caa863
jackmorrison <jsm1317@ic.ac.uk>
2020-07-04 22:55:57 +0100

    Adds recent icons pulsing to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
@@ -16,6 +16,23 @@ import socials from "../data/socials.json"
 library.add(fab)
 
 export default () => {
+  const [recents, setRecents] = useState([])
+
+  useEffect(() => {
+    var url =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:8080"
+        : "https://api.jackmorrison.xyz"
+
+    fetch(url + "/getRecents")
+      .then(response => response.json())
+      .then(result => {
+        setRecents(result)
+        console.log(result)
+      })
+      .catch(error => console.log("error", error))
+  }, [])
+
   return (
     <Layout>
       <SEO title="Home" />
@@ -70,7 +87,12 @@ export default () => {
       <div className="is-light-grey-bg is-black ">
         <div className="row container-small pad-20-t pad-3-lr pad-10-b ">
           <div className="col-xs-12">
-            <h2 className="text-align-center">Where else can you reach me?</h2>
+            <h2 className="text-align-center margin-0-b">
+              Where else can you reach me?
+            </h2>
+            <p className="text-align-center margin-0-t">
+              The pulsing icons have had recent activity!
+            </p>
             <div className="text-align-center row content-center">
               {socials
                 .filter(item => item.home)
@@ -79,11 +101,24 @@ export default () => {
                     href={item.url}
                     className="is-black pad-2 col-xs-4 col-sm-1"
                   >
-                    <FontAwesomeIcon
-                      icon={["fab", item.icon]}
-                      size="2x"
-                      className="grow-5"
-                    />
+                    {recents[item.identifier] ? (
+                      //Make flaming
+                      <FontAwesomeIcon
+                        icon={["fab", item.icon]}
+                        size="2x"
+                        className={
+                          "grow-5 flame " + item.identifier + "-colour"
+                        }
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        icon={["fab", item.icon]}
+                        size="2x"
+                        className={
+                          "grow-5  " + item.identifier + "-colour-hover"
+                        }
+                      />
+                    )}
                   </OutboundLink>
                 ))}
             </div>
