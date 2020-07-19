0058e585aa46fec1d3a4d0d23aaa5832f597770b
jackmorrison <jsm1317@ic.ac.uk>
2020-07-19 23:27:00 +0100

    Adds blog posts to series pages

--- src/templates/series.js
+++ src/templates/series.js
@@ -12,8 +12,9 @@ import SEO from "../components/seo"
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const { series, blogposts } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = series
+  const posts = blogposts.edges
   return (
     <Layout>
       <SEO title={frontmatter.title} />
@@ -32,8 +33,33 @@ export default function Template({
             </h6>
             <h6 className="subtitle margin-3-b">{frontmatter.date}</h6>
             <div className="line margin-5-tb is-red" />
-            <div className="blog" dangerouslySetInnerHTML={{ __html: html }} />
+            {/* <div className="blog" dangerouslySetInnerHTML={{ __html: html }} /> */}
           </div>
+        </div>
+        <div className="row container">
+          {posts &&
+            posts.map(({ node: post }) => (
+              <Link to={"/" + post.frontmatter.fullurl} className="" id="path">
+                <div className="grow row margin-5-b">
+                  <div className="col-xs-12 margin-5-t">
+                    <h3 className="margin-0 is-red">
+                      {post.frontmatter.series}: Part {post.frontmatter.entry}
+                    </h3>
+                    <h1 className="margin-0 is-medium-blue">
+                      {post.frontmatter.title}
+                    </h1>
+                    <p className="margin-0 margin-2-b is-black">
+                      {post.frontmatter.date}
+                    </p>
+                    <p className="margin-0 margin-1-b is-black bold is-red pad-2-b">
+                      {post.frontmatter.description}
+                    </p>
+                    <div className="line-sm is-black margin-3-b" />
+                    <p className="margin-0 is-black">{post.excerpt}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
         </div>
       </div>
     </Layout>
@@ -41,7 +67,7 @@ export default function Template({
 }
 export const pageQuery = graphql`
   query($slug: String!, $baseurl: String!) {
-    markdownRemark(
+    series: markdownRemark(
       frontmatter: { slug: { eq: $slug }, baseurl: { eq: $baseurl } }
     ) {
       html
@@ -50,6 +76,27 @@ export const pageQuery = graphql`
         slug
         title
         description
+      }
+    }
+    blogposts: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: {
+        frontmatter: { blogseries: { eq: $slug }, baseurl: { eq: $baseurl } }
+      }
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 300)
+          id
+          frontmatter {
+            title
+            series
+            entry
+            description
+            date(formatString: "MMMM DD, YYYY")
+            fullurl
+          }
+        }
       }
     }
   }
