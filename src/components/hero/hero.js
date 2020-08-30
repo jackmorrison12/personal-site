7167aa29e8efa4fa6cc4e4e6bd1b0f169e0ea1e8
jackmorrison <jsm1317@ic.ac.uk>
2020-08-30 01:00:28 +0100

    Updates gradient of header

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -6,13 +6,13 @@ import Socials from "./socials"
 
 export default props => (
   <>
-    <div className="is-red-bg">
-      <div className="container-small row hero">
+    <div className="hero">
+      <div className="container-small row">
         <div className="col-xs-12 text-align-center">
-          <h1 className="title margin-0 pad-15-t">
+          <h1 className="h-title margin-0 pad-15-t">
             <Twemoji svg text="Hey 👋 I'm Jack" />{" "}
           </h1>
-          <h2 className="subtitle margin-0">
+          <h2 className="h-subtitle margin-0">
             <br />
             I'm a computing student at{" "}
             <OutboundLink
@@ -25,16 +25,21 @@ export default props => (
             </OutboundLink>
           </h2>
           <div className="pad-5-t">
-            <Socials recents={props.recents} />
+            <Socials recents={props.recents} showRecents={false} />
           </div>
         </div>
       </div>
     </div>
     <div class="wave-container">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
+        <defs>
+          <linearGradient id="g1">
+            <stop stop-color="#833ab4" />
+            <stop offset="1" stop-color="#fd1d1d" />
+          </linearGradient>
+        </defs>
         <path
-          fill="#f4442e"
-          fill-opacity="1"
+          fill="url(#g1)"
           d="M0,128L80,117.3C160,107,320,85,480,96C640,107,800,149,960,154.7C1120,160,1280,128,1360,112L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
         ></path>
       </svg>
