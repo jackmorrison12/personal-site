390d15a3dc66de3a95c7cd3a8e48daa2844a38c6
jackmorrison <jsm1317@ic.ac.uk>
2020-08-30 01:16:46 +0100

    Exports wave to separate file

---
+++ src/components/hero/wave.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
+    <defs>
+      <linearGradient id="g1">
+        <stop stop-color="#833ab4" />
+        <stop offset="1" stop-color="#fd1d1d" />
+      </linearGradient>
+    </defs>
+    <path
+      fill="url(#g1)"
+      d="M0,128L80,117.3C160,107,320,85,480,96C640,107,800,149,960,154.7C1120,160,1280,128,1360,112L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+    ></path>
+  </svg>
+)
