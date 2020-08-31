ec4f6832cfa69e862ade51799516c2e91e42eb03
jackmorrison <jsm1317@ic.ac.uk>
2020-08-31 12:48:27 +0100

    Removes recents from homepage socials

--- src/components/hero/socials.js
+++ src/components/hero/socials.js
@@ -32,20 +32,11 @@ export default props => {
             href={item.url}
             className="is-white-always pad-2 col-xs-4 col-sm-1"
           >
-            {recents[item.identifier] ? (
-              //Make flaming
-              <FontAwesomeIcon
-                icon={["fab", item.icon]}
-                size="2x"
-                className={"grow-5 flame " + item.identifier + "-colour"}
-              />
-            ) : (
-              <FontAwesomeIcon
-                icon={["fab", item.icon]}
-                size="2x"
-                className={"grow-5  " + item.identifier + "-colour-hover"}
-              />
-            )}
+            <FontAwesomeIcon
+              icon={["fab", item.icon]}
+              size="2x"
+              className={"grow-5  " + item.identifier + "-colour-hover"}
+            />
           </OutboundLink>
         ))}
     </div>
