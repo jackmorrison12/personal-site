44263ae6073c26c054a0754aefeb2c4dc281a7ea
jackmorrison <jsm1317@ic.ac.uk>
2020-09-17 22:56:09 +0100

    Removes form from home page

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -24,40 +24,5 @@ export default () => (
         <WritingPreviews />
       </div>
     </div>
-    <form
-      method="post"
-      netlify-honeypot="bot-field"
-      data-netlify="true"
-      name="New Homepage"
-      className="row text-align-center is-red-bg pad-5"
-    >
-      <h2 className="col-xs-12">
-        Please give me some feedback on the new homepage
-      </h2>
-      <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
-      <label className="col-xs-12 col-md-6 left-xs right-md">Name</label>
-      <input type="text" name="name" id="name" className="col-xs-12 col-md-4" />
-      <label className="col-xs-12 col-md-6 left-xs right-md">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        className="col-xs-12 col-md-4"
-      />
-      <label className="col-xs-12 col-md-6 left-xs right-md">
-        What do you think of this new home page?
-      </label>
-      <textarea
-        name="message"
-        id="message"
-        rows="5"
-        className="col-xs-12 col-md-4"
-      />
-      <div className="col-xs-5"></div>
-      <button type="submit" className="col-xs-2 margin-3-tb">
-        Send
-      </button>
-    </form>
   </Layout>
 )
