e25f6df43cf77af243ff01b137478c8c22b45047
jackmorrison <jsm1317@ic.ac.uk>
2020-09-21 23:58:23 +0100

    Update tag page to split by type of item

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -9,30 +9,119 @@ import SEO from "../components/seo"
 const Tags = ({ pageContext, data }) => {
   const { tag } = pageContext
   const { edges, totalCount } = data.allMarkdownRemark
-  const tagHeader = `${totalCount} post${
+  const tagHeader = `${totalCount} item${
     totalCount === 1 ? "" : "s"
   } tagged with "${tag}"`
+  const projCount = edges.filter(
+    item => item.node.frontmatter.type === "project"
+  ).length
+  const articleCount = edges.filter(
+    item => item.node.frontmatter.type === "article"
+  ).length
+  const blogCount = edges.filter(item => item.node.frontmatter.type === "blog")
+    .length
+  const seriesCount = edges.filter(
+    item => item.node.frontmatter.type === "series"
+  ).length
 
   return (
     <Layout>
       <SEO title={tagHeader} />
-      <div className="is-grey is-light-grey-bg">
-        <div className="row container pad-3-lr">
-          <div className="col-xs-12">
-            <h1 className="title margin-3-t margin-0-b">{tagHeader}</h1>
-            <div>
-              <ul>
-                {edges.map(({ node }) => {
-                  const { title, fullurl } = node.frontmatter
-                  return (
-                    <li key={fullurl}>
-                      <Link to={fullurl}>{title}</Link>
-                    </li>
-                  )
-                })}
-              </ul>
+      <div className="is-grey is-light-grey-bg pad-10-tb pad-3-lr">
+        <div className="container-small ">
+          <div className="row">
+            <div className="col-xs-12">
+              <h1 className="title title-borders margin-0-t">
+                {totalCount +
+                  " item" +
+                  (totalCount === 1 ? "" : "s") +
+                  " tagged with "}
+                <span className="is-primary">"{tag}"</span>
+              </h1>
             </div>
           </div>
+          {projCount > 0 ? (
+            <div className="row">
+              <div className="col-xs-12">
+                <h1>
+                  {projCount} Project{projCount === 1 ? "" : "s"}
+                </h1>
+                {edges
+                  .filter(item => item.node.frontmatter.type === "project")
+                  .map(({ node }) => {
+                    const { title, fullurl } = node.frontmatter
+                    return (
+                      <p>
+                        <Link to={fullurl}>{title}</Link>
+                      </p>
+                    )
+                  })}
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+          {articleCount > 0 ? (
+            <div className="row">
+              <div className="col-xs-12">
+                <h1>
+                  {articleCount} Article{articleCount === 1 ? "" : "s"}
+                </h1>
+                {edges
+                  .filter(item => item.node.frontmatter.type === "article")
+                  .map(({ node }) => {
+                    const { title, fullurl } = node.frontmatter
+                    return (
+                      <p>
+                        <Link to={fullurl}>{title}</Link>
+                      </p>
+                    )
+                  })}
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+          {blogCount > 0 ? (
+            <div className="row">
+              <div className="col-xs-12">
+                <h1>
+                  {blogCount} Blog Post{blogCount === 1 ? "" : "s"}
+                </h1>
+                {edges
+                  .filter(item => item.node.frontmatter.type === "blog")
+                  .map(({ node }) => {
+                    const { title, fullurl } = node.frontmatter
+                    return (
+                      <p>
+                        <Link to={fullurl}>{title}</Link>
+                      </p>
+                    )
+                  })}
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+          {seriesCount > 0 ? (
+            <div className="row">
+              <div className="col-xs-12">
+                <h1>{seriesCount} Blog Series</h1>
+                {edges
+                  .filter(item => item.node.frontmatter.type === "series")
+                  .map(({ node }) => {
+                    const { title, fullurl } = node.frontmatter
+                    return (
+                      <p>
+                        <Link to={fullurl}>{title}</Link>
+                      </p>
+                    )
+                  })}
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </Layout>
@@ -65,7 +154,10 @@ export const pageQuery = graphql`
   query($tag: String) {
     allMarkdownRemark(
       limit: 2000
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: {
+        fields: [frontmatter___type, frontmatter___date]
+        order: [ASC, DESC]
+      }
       filter: { frontmatter: { tags: { in: [$tag] } } }
     ) {
       totalCount
@@ -74,6 +166,7 @@ export const pageQuery = graphql`
           frontmatter {
             title
             fullurl
+            type
           }
         }
       }
